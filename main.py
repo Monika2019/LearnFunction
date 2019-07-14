18ae85842145c01231fc59c7692427543149a34d
Monika2019 <52123265+Monika2019@users.noreply.github.com>
2019-07-14 15:03:10 +0800

    01

---
+++ main.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+from my_modules import *
+
+prt_cut_line(1)
+a = 1
+b = 2
+c = a + b
+print(c)
+
+c = add(a, b)
+print(c)
+
+prt_cut_line(2)
+s = cal_rect_area(10, 100)
+print(s)
---
+++ my_modules.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+
+def add(a, b):
+    '这是第一个函数'
+    sum=a+b
+    return sum
+
+def cal_rect_area(lenth,width):
+    '计算长方形面积'
+    area = lenth * width
+    return area
+
+def prt_cut_line( sn ):
+    print('\n-----------cutting line (%d)------------'%(sn))
+    return
